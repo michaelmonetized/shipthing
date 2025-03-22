d4db108104723eeb68eb41115b2c7ae0e79e209e
Michael Monetized <michaelmonetized@gmail.com>
2025-03-22 03:13:13 -0400

    clerk

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { SignedIn, SignedOut, SignIn, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
@@ -25,6 +26,14 @@ export default function Home() {
           </li>
         </ol>
 
+        <div className="flex gap-4 items-center flex-col sm:flex-row">
+          <SignedOut>
+            <SignIn routing="hash" />
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </div>
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,7 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 const isPublicRoute = createRouteMatcher([
   "/login(.*)",
   "/sentry-example-page(.*)",
-  "/",
+  "/(.*)",
 ]);
 
 export default clerkMiddleware(async (auth, req) => {
